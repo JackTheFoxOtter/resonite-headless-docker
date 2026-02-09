855d91864561d01ec36e5d775e7d6e729de12d3a
JackTheFoxOtter <jackthefoxotter@gmail.com>
2026-02-09 16:18:04 +0100

    Added logging

--- start_resonite.sh
+++ start_resonite.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd ${STEAMAPPDIR}/Headless
+echo Launch command: \"dotnet Resonite.dll -HeadlessConfig /Config/Config.json -LogsPath /Logs/ ${LAUNCHARGS}\"
 exec dotnet Resonite.dll -HeadlessConfig /Config/Config.json -LogsPath /Logs/ ${LAUNCHARGS}

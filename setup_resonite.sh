217da5f62673fe5c6dc438f059f794d5feab7476
JackTheFoxOtter <jackthefoxotter@gmail.com>
2026-07-26 16:57:25 +0200

    Change order again

--- setup_resonite.sh
+++ setup_resonite.sh
@@ -14,8 +14,8 @@ else
 fi
 
 bash "${STEAMCMDDIR}/steamcmd.sh" \
-    +force_install_dir ${STEAMAPPDIR} \
     +@sSteamCmdForcePlatformType windows \
+    +force_install_dir ${STEAMAPPDIR} \
     +login ${STEAMLOGIN} \
     +app_license_request ${STEAMAPPID} \
     +app_update ${STEAMAPPID} ${BETA_ARGS} validate \
